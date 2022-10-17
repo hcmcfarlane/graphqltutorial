8c965dd3e75db3352b7798878929c8c957c7e16d
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-17 20:26:43 +0100

    add project intro and some styling to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Animes from "../Animes/Animes.js";
 import emoji from "../../android-chrome-192x192.png";
 
 function App() {
@@ -6,19 +7,38 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<h1>
-					<img src={emoji} height="60px" /> GraphQL Tutorial
+					<img src={emoji} height="60px" alt="" /> GraphQL Tutorial
 				</h1>
 			</header>
 			<main>
 				<p>
-					Following the tutorial at{" "}
+					A very quick first look at GraphQL. I followed the tutorial
+					at{" "}
 					<a
 						href="https://www.freecodecamp.org/news/graphql-for-front-end-developers/"
 						target="_blank"
 						rel="noopener noreferrer">
-						https://www.freecodecamp.org/news/graphql-for-front-end-developers/
+						freeCodeCamp
 					</a>
+					. Using this tutorial I learned:
 				</p>
+				<ul>
+					<li>GraphQL is a query language and runtime</li>
+					<li>
+						GraphQL helps solve problems of under- and over-fetching
+					</li>
+					<li>Using ApolloClient to set up a client</li>
+					<li>
+						Wrapping your root render in ApolloProvider to pass the
+						client as context to your whole app
+					</li>
+					<li>
+						Setting up a query to give back the data structure you
+						want
+					</li>
+					<li>Adding variables to a query</li>
+				</ul>
+				<Animes />
 			</main>
 		</div>
 	);
